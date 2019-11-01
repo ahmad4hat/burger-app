0e338fcd1e179ae586a26ca354535d4b6193a994
ahmad4hat <rappar.stand@gmail.com>
2019-11-01 23:03:26 +0600

    oder fetching and order outputing + error handler added in orders component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,32 @@ import React from 'react';
 import CssClass from './Order.module.css';
 
 const Order = (props) => {
+    const ingredients=[];
+
+    for(let ingredientName in props.ingredients){
+        ingredients.push({
+            name :ingredientName,
+            amount:props.ingredients[ingredientName]
+        })
+    }
+
+    const ingredientOutput=ingredients.map(ig=>{
+        return(<span 
+            style={{
+             textTransform :"capitalize",
+             display:'inline-block',
+             margin:'0px 8px',
+             border :'1px solid #ccc',
+             padding :'5px'
+
+            }}
+            key={ig.name}>{ig.name} ({ig.amount}) </span>);
+    })
+
     return (
         <div className={CssClass.Order}>
-            <p>Ingredient salad : (1)</p>
-            <p> Price :<strong>Taka 500 </strong></p>
+            <p>Ingredient :{ingredientOutput}</p>
+            <p> Price :<strong>TK {(+props.price).toFixed(2)} </strong></p>
         </div>
     );
 }
