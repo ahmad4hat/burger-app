68aef4cbca6565906c133cd981fea49cad7bbba2
ahmad4hat <rappar.stand@gmail.com>
2019-11-15 10:12:42 +0600

    enviroment variable added

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV==='development'? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__:null || compose;
 
+
+console.log(process.env.NODE_ENV);
 const rootReducer=combineReducers({
   burgerBuilder:burgerBuilderReducer,
   order:orderReducer,
